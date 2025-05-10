0bc71be0c61ded5d9383ec8dc190da1155845cf4
Sutyinszki János <janos@Janos-MacBook-Air.local>
2025-05-10 18:25:50 +0200

    fix manifest

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-gray-100">
       <div className="bg-white p-16 rounded-xl shadow-2xl text-center space-y-6">
-        <h1 className="text-3xl font-bold text-gray-800">Potmeter Value</h1>
+        <h1 className="text-3xl font-bold text-gray-800">Workshop Happiness</h1>
         <div className="text-8xl font-extrabold text-blue-600">{potmeterValue}%</div>
         <div className="flex items-center justify-center space-x-6">
           <button 
